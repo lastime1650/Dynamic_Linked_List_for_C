f04b47664cc21f821e1e33890104600420b854ee
lastime1650 <54367564+lastime1650@users.noreply.github.com>
2024-07-19 20:14:13 +0900

    Update main.c

--- Source/main.c
+++ Source/main.c
@@ -9,13 +9,15 @@ int main() {
 
     hMutex = CreateMutex(NULL, FALSE, NULL);
     if (hMutex == NULL) {
-        printf("π¬≈ÿΩ∫ ª˝º∫ Ω«∆–: %d\n", GetLastError());
+        printf("ÎÆ§ÌÖçÏä§ ÏÉùÏÑ± Ïã§Ìå®: %d\n", GetLastError());
         return 1;
     }
 
     ULONG32 test_parm1 = 1;
 
     CreateThread(NULL, 0, test, &test_parm1, 0, NULL);
+
+    /*
     ULONG32 test_parm2 = 2;
     CreateThread(NULL, 0, test, &test_parm2, 0, NULL);
     
@@ -23,7 +25,7 @@ int main() {
     CreateThread(NULL, 0, test, &test_parm3, 0, NULL);
     ULONG32 test_parm4 = 4;
     CreateThread(NULL, 0, test, &test_parm4, 0, NULL);
-    /*
+    
     test_parm =5;
     CreateThread(NULL, 0, test, &test_parm, 0, NULL);
     test_parm = 6;
@@ -37,39 +39,69 @@ int main() {
     
     while (1) {
     }
-    printf("º∫∞¯\n");
+    printf("ÏÑ±Í≥µ\n");
 	return 0;
 }
 
+PDynamic_NODE make_linked_list_for_test(ULONG32 start_i, ULONG32 end_i, ULONG32 Node_Search_VALUE);
+
 DWORD WINAPI test(LPVOID lpParam) {
 
-    ULONG64 index = 0;
-    PDynamic_NODE Section_Start_Node = NULL;
 
-    for (ULONG32 i = 0; i < 2; i++) {
 
-        ULONG32 sample1 = i;
 
-        if (i == 0) {
-            Build_up_Node((PUCHAR)&sample1, sizeof(sample1), TRUE, &index, &Section_Start_Node);
-            printf("Build_up_Node ¿∏∑Œ∫Œ≈Õ πﬁ¿∫ Ω√¿€¡÷º“ %p \n", Section_Start_Node);
-            continue;
-        }
-        
-        Build_up_Node((PUCHAR) & sample1, sizeof(sample1), FALSE,&index, NULL);
+    PDynamic_NODE Section_Start_Node1 = make_linked_list_for_test(0,10,'D1');
+
+
+    PDynamic_NODE res1 = Get_Node_1Dim(Section_Start_Node1, 2);
+    if (res1) {
+        printf("[1ÏÇ≠Ï†úÏ†Ñ] Îç∞Ïù¥ÌÑ∞ Ï∂îÏ∂ú -> %d \n", res1->DATA_SIZE);//3Î≤àÏ®∞ ÎÖ∏Îìú Í∞í
     }
 
-    PDynamic_NODE res = Get_Node(Section_Start_Node, 2);
-    if (res) {
-        printf("[ªË¡¶¿¸] µ•¿Ã≈Õ √ﬂ√‚ -> %d \n", res->DATA_SIZE);//3π¯§ä ≥ÎµÂ ∞™
+    ///
+
+    PDynamic_NODE Section_Start_Node2 = make_linked_list_for_test(0, 10, 'D2');
+
+    PDynamic_NODE res2 = Get_Node_1Dim(Section_Start_Node2, 2);
+    if (res2) {
+        printf("[2ÏÇ≠Ï†úÏ†Ñ] Îç∞Ïù¥ÌÑ∞ Ï∂îÏ∂ú -> %d \n", res2->DATA_SIZE);//3Î≤àÏ®∞ ÎÖ∏Îìú Í∞í
     }
 
+    ///
+
+    PDynamic_NODE Section_Start_Node3 = make_linked_list_for_test(0, 10, 'D1');
+
+
+    PDynamic_NODE res3 = Get_Node_1Dim(Section_Start_Node3, 2);
+    if (res3) {
+        printf("[3ÏÇ≠Ï†úÏ†Ñ] Îç∞Ïù¥ÌÑ∞ Ï∂îÏ∂ú -> %d \n", res3->DATA_SIZE);//3Î≤àÏ®∞ ÎÖ∏Îìú Í∞í
+    }
    
+    print_node();
 
-    Remove_Node(Section_Start_Node);
+    //Remove_Node(Section_Start_Node);
 
-    // Section_Start_Node ¡÷º“∞™¿∫  ¥ı¿ÃªÛ ¿Ø»ø«œ¡ˆ æ ¿Ω 
-    printf(" %p is_valid_address ¿Ø»ø∞·∞˙ -> %d \n", Section_Start_Node, is_valid_address(Section_Start_Node)); // 0
+    // Section_Start_Node Ï£ºÏÜåÍ∞íÏùÄ  ÎçîÏù¥ÏÉÅ Ïú†Ìö®ÌïòÏßÄ ÏïäÏùå 
+    //printf(" %p is_valid_address Ïú†Ìö®Í≤∞Í≥º -> %d \n", Section_Start_Node, is_valid_address(Section_Start_Node)); // 0
     
     return 0;
+}
+
+PDynamic_NODE make_linked_list_for_test(ULONG32 start_i, ULONG32 end_i, ULONG32 Node_Search_VALUE) {
+    PDynamic_NODE Section_Start_Node = NULL;
+
+    for (start_i = 0; start_i < end_i; start_i++) {
+
+        ULONG32 sample1 = start_i;
+
+        if (start_i == 0) {
+            Build_up_Node((PUCHAR)&sample1, sizeof(sample1), TRUE, &Section_Start_Node, Node_Search_VALUE);
+            //printf("Build_up_Node ÏúºÎ°úÎ∂ÄÌÑ∞ Î∞õÏùÄ ÏãúÏûëÏ£ºÏÜå %p \n", Section_Start_Node);
+            continue;
+        }
+
+        Build_up_Node((PUCHAR)&sample1, sizeof(sample1), FALSE, &Section_Start_Node, Node_Search_VALUE);
+    }
+
+    return Section_Start_Node;
 }
